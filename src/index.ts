34fe9ca887dbcba50b3392ee7e248b1585e4f288
Rajneesh Mishra <rajneesh.mishra9616@gmail.com>
2024-06-06 14:58:00 +0530

    Initial commit, setting up db server and TS

---
+++ src/index.ts
@@ -0,0 +1,23 @@
+import pg from "pg";
+const { Client } = pg;
+
+const client = new Client({
+  connectionString: "postgresql://postgres:12345@localhost/postgres",
+});
+
+client.connect();
+
+async function createUsersTable() {
+  const result = await client.query(`
+    CREATE TABLE users (
+        id SERIAL PRIMARY KEY,
+        username VARCHAR(50) UNIQUE NOT NULL,
+        email VARCHAR(255) UNIQUE NOT NULL,
+        password VARCHAR(255) NOT NULL,
+        created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
+    )
+  `);
+  console.log(result);
+}
+
+createUsersTable();
